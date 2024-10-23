77715674b59256c2dc538692664df20424de03f5
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-10-23 19:25:57 +0600

    parallelogram added

---
+++ scripts/parallelogram.js
@@ -0,0 +1,15 @@
+function calculateParallelogram(){
+    const parallelogramBase = document.getElementById('parallelogram-base');
+    const parallelogramBaseText = parallelogramBase.value;
+    const base = parseFloat(parallelogramBaseText);
+
+    const parallelogramHeight = document.getElementById('parallelogram-height');
+    const parallelogramHeightText = parallelogramHeight.value;
+    const height = parseFloat(parallelogramHeightText);
+
+    const area = base * height;
+
+    const parallelogramArea = document.getElementById('parallelogram-area');
+    parallelogramArea.innerText = area;
+
+}
--- scripts/rectangleArea.js
+++ scripts/rectangleArea.js
@@ -1,5 +1,17 @@
 function calculateRectangleArea(){
-
+    // Get the rectangle width
+    const recTangleWidth = document.getElementById('rectangle-width');
+    const recTangleWidthText = recTangleWidth.value;
+    const width = parseFloat(recTangleWidthText);
     
+    // Get the Rectangle length
+    const rectangleLength = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLength.value;
+    const length = parseFloat(rectangleLengthText)
+
+    // Calculate rectangle area
+    const area = width * length;
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
 
 }
